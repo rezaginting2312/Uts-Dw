e03d350f0732fdffac7a37cda06af6498c9503a1
rezaginting2312 <gintingreza557@gmail.com>
2024-11-04 13:05:37 +0700

    Add files via upload

---
+++ dw uts/script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+var modal = document.getElementById("myModal");
+var btn = document.querySelector(".action-btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
